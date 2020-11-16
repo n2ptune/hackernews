9b7c467ddeb7402c6d03e46e8cdd50f8236f09a5
n2ptune <n2ptune@outlook.com>
2020-11-16 19:42:14 +0900

    adding pages with router

---
+++ src/routes/index.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Main from '../views/Main'
+import Top from '../views/Top'
+import Best from '../views/Best'
+import Ask from '../views/Ask'
+import Job from '../views/Job'
+
+const Router: React.FC = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" exact component={Main} />
+      <Route path="/top" component={Top} />
+      <Route path="/best" component={Best} />
+      <Route path="/ask" component={Ask} />
+      <Route path="/job" component={Job} />
+    </Switch>
+  </BrowserRouter>
+)
+
+export default Router
