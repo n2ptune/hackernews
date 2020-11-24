6bcb27e05693d15dc379ce690ada75b4ed224e83
n2ptune <n2ptune@outlook.com>
2020-11-24 14:33:21 +0900

    push raw html with dangeroslySetInnerHTML

--- src/components/detail/Comments.tsx
+++ src/components/detail/Comments.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import styled from 'styled-components'
+import { distanceDateFromNow } from '../../api/utils'
 
 interface CommentsComponentProps {
   comments: Array<{ [key: string]: any }>
@@ -25,7 +27,24 @@ const ItemWrapper = styled.ul`
   margin: 0;
 `
 
-const Item = styled.li``
+const Item = styled.li`
+  & .user {
+    text-decoration: underline;
+    margin-right: 5px;
+  }
+
+  & .date {
+    font-size: 0.9rem;
+    font-weight: bold;
+    color: rgba(0, 0, 0, 0.7);
+  }
+
+  & a[rel='nofollow'] {
+    color: var(--head-color);
+    font-weight: bold;
+    text-decoration: underline;
+  }
+`
 
 const CommentItemWrapper: React.FC<CommentsComponentProps> = ({ comments }) => {
   return (
@@ -39,8 +58,16 @@ const CommentItemWrapper: React.FC<CommentsComponentProps> = ({ comments }) => {
 
 const CommentItem: React.FC<{ comment: any }> = ({ comment }) => {
   return (
-    <Item style={{ paddingLeft: `${comment.level * 6}px` }}>
-      {comment.id}, {comment.level}
+    <Item style={{ paddingLeft: `${comment.level * 7}px` }}>
+      <p>
+        <span className="user">
+          <Link to={`/user/${comment.user}`}>{comment.user}</Link>
+        </span>
+        <span className="date">
+          {distanceDateFromNow(comment.time, Date.now())}
+        </span>
+      </p>
+      {<p dangerouslySetInnerHTML={{ __html: comment.content }}></p>}
       <>
         {comment.comments && <CommentItemWrapper comments={comment.comments} />}
       </>
